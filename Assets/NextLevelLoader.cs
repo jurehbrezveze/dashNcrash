ef56847806734395896e89d0e9baef25e2a9b33b
ZZedYT <alexlevjesrcni@gmail.com>
2025-02-09 11:49:59 +0100

    stuff works now 1 and 2 lvl are done!

--- Assets/NextLevelLoader.cs
+++ Assets/NextLevelLoader.cs
@@ -3,8 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class NextLevelLoader : MonoBehaviour
 {
-    public GameObject player;  // Assign the player object in the Inspector
-    public float delay = 5f;   // Delay before loading the next level
+    public GameObject player;          // Assign the player object in the Inspector
+    public string nextSceneName = "";  // Name of the next scene to load (assign in the Inspector)
+    public float delay = 5f;           // Delay before loading the next level
 
     private bool playerTouched = false;
 
@@ -13,7 +14,7 @@ public class NextLevelLoader : MonoBehaviour
         if (other.gameObject == player && !playerTouched)
         {
             playerTouched = true;
-            Debug.Log("Player touched the Finish. Next level loading in 5 seconds...");
+            Debug.Log($"Player touched the Finish. Next level '{nextSceneName}' loading in {delay} seconds...");
             StartCoroutine(LoadNextLevelAfterDelay());
         }
     }
@@ -21,6 +22,14 @@ public class NextLevelLoader : MonoBehaviour
     private System.Collections.IEnumerator LoadNextLevelAfterDelay()
     {
         yield return new WaitForSeconds(delay);
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+        if (!string.IsNullOrEmpty(nextSceneName))
+        {
+            SceneManager.LoadScene(nextSceneName);
+        }
+        else
+        {
+            Debug.LogError("Next scene name is not assigned!");
+        }
     }
 }
